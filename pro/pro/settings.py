dac351151f8bbdf15fa0296d31474005b3fdcd06
MUHAMMED FASIL T K <93210018+fasiltk@users.noreply.github.com>
2024-07-17 07:56:38 +0530

    Update settings.py
    Deleted email

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -124,9 +124,3 @@ STATICFILES_DIRS=[
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'muhammedfasil32745@gmail.com'
-EMAIL_HOST_PASSWORD = 'segtdapmlofrfker'
